90bdec5a45155a0d395e24f31cc1c64a0de1838d
HAROLD <havilez@gmail.com>
2015-06-19 23:14:59 -0400

    added orig karma.conf.js used when running karma standalone

--- karma.conf.js
+++ karma.conf.js
@@ -33,7 +33,7 @@ module.exports = function(config) {
 
 
     // test results reporter to use
-    // possible values: 'dots', 'progress'
+    // possiblegit values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
     reporters: ['progress'],
 
